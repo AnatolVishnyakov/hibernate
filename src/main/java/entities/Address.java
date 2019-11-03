0e100edf649704c3d2d406b834636ed4c4f42a3a
Anatol V <malfenius@gmail.com>
2019-11-03 21:47:16 +0300

    Аннотация Transient (не сохранять поле в БД)

--- src/main/java/entities/Address.java
+++ src/main/java/entities/Address.java
@@ -1,10 +1,15 @@
 package entities;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
+import javax.persistence.Transient;
 
 @Embeddable
+@Access(AccessType.FIELD)
 public class Address {
-    private String city;
+    private transient String city;
+    @Transient
     private String street;
     private String house;
 
@@ -15,30 +20,6 @@ public class Address {
     public Address(String city, String street, String house) {
         this.city = city;
         this.street = street;
-        this.house = house;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouse() {
-        return house;
-    }
-
-    public void setHouse(String house) {
         this.house = house;
     }
 
