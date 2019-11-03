550854c448dd59dfd2f60dd77005a9df76e93bab
Anatol V <malfenius@gmail.com>
2019-11-03 17:15:25 +0300

    Маппинг сущности Student через аннотации

--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -1,8 +1,14 @@
 package entities;
 
-public class Student {
-    private int id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private int id;
     private String name;
 
     public Student() {
