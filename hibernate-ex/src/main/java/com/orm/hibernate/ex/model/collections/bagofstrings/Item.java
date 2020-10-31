0af4e31424751fabaf6b0a91d7371d8977d7b376
Anatol V <malfenius@gmail.com>
2020-10-31 20:18:20 +0300

    Отображение контейнера идентификаторов

---
+++ hibernate-ex/src/main/java/com/orm/hibernate/ex/model/collections/bagofstrings/Item.java
@@ -0,0 +1,64 @@
+package com.orm.hibernate.ex.model.collections.bagofstrings;
+
+import com.orm.hibernate.ex.model.EntitySaver;
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+//@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // создает таблицу hibernate_sequence
+    private Long id;
+    @ElementCollection
+    @CollectionTable(name = "IMAGE")
+    @Column(name = "FILENAME")
+    @CollectionId(
+            columns = {@Column(name = "IMAGE_ID")},
+            type = @Type(type = "long"),
+            generator = "id_generator"
+    )
+    private Collection<String> images = new ArrayList<>();
+    @Column(name = "NAME")
+    private String name;
+
+    public Item() {
+    }
+
+    public Collection<String> getImages() {
+        return images;
+    }
+
+    public void setImages(Collection<String> images) {
+        this.images = images;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", images=" + images +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        EntitySaver.save(entityManager -> {
+            final Item item = new Item();
+            item.getImages().add("File-1");
+            item.setName("file test");
+            entityManager.persist(item);
+        });
+    }
+}
