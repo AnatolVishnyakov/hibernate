5b31ca9ddfba34cedfda12dac80bb7508f91d517
Anatol V <malfenius@gmail.com>
2020-10-31 18:33:36 +0300

    Отображение множества

---
+++ hibernate-ex/src/main/java/com/orm/hibernate/ex/model/collections/setofstrings/Item.java
@@ -0,0 +1,54 @@
+package com.orm.hibernate.ex.model.collections.setofstrings;
+
+import com.orm.hibernate.ex.model.EntitySaver;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+//@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // создает таблицу hibernate_sequence
+    private Long id;
+    @ElementCollection
+    @CollectionTable(name = "IMAGE", joinColumns = @JoinColumn(name = "ITEM_ID"))
+    @Column(name = "FILENAME")
+    private Set<String> images = new HashSet<>();
+    @Column(name = "NAME")
+    private String name;
+
+    public Set<String> getImages() {
+        return images;
+    }
+
+    public void setImages(Set<String> images) {
+        this.images = images;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", images=" + images +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        EntitySaver.save(entityManager -> {
+            final Item item = new Item();
+            item.getImages().add("File-1");
+            item.setName("file test");
+            entityManager.persist(item);
+        });
+    }
+}
