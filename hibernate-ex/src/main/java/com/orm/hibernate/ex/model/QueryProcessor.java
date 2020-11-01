41a1f4dd3ee23db69bf53b729b696c746e564749
Anatol V <malfenius@gmail.com>
2020-11-01 15:28:11 +0300

    Добавил метод возвращающий результат

--- hibernate-ex/src/main/java/com/orm/hibernate/ex/model/QueryProcessor.java
+++ hibernate-ex/src/main/java/com/orm/hibernate/ex/model/QueryProcessor.java
@@ -5,9 +5,10 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
-public class EntitySaver {
-    public static void save(Consumer<EntityManager> consumer) {
+public class QueryProcessor {
+    public static void process(Consumer<EntityManager> consumer) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateEx");
 
         EntityManager em = emf.createEntityManager();
@@ -18,5 +19,19 @@ public class EntitySaver {
 
         tx.commit();
         em.close();
+    }
+
+    public static <R> R process(Function<EntityManager, R> function) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateEx");
+
+        EntityManager em = emf.createEntityManager();
+        final EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        final R result = function.apply(em);
+
+        tx.commit();
+        em.close();
+        return result;
     }
 }
