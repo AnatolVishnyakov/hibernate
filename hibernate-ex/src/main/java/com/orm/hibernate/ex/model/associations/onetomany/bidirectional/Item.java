a3c34f1a3dcf2366dd6906c3c42a6e6a943d2f76
Anatol V <malfenius@gmail.com>
2020-11-02 23:04:48 +0300

    Раздельное урпавление сущностей

--- hibernate-ex/src/main/java/com/orm/hibernate/ex/model/associations/onetomany/bidirectional/Item.java
+++ hibernate-ex/src/main/java/com/orm/hibernate/ex/model/associations/onetomany/bidirectional/Item.java
@@ -3,6 +3,7 @@ package com.orm.hibernate.ex.model.associations.onetomany.bidirectional;
 import com.orm.hibernate.ex.model.QueryProcessor;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -52,10 +53,26 @@ public class Item {
         Item item = new Item("test-" + new Random().nextInt(10_000));
         QueryProcessor.process(entityManager -> {
             entityManager.persist(item);
-            entityManager.persist(new Bid(1.0f, item));
-            entityManager.persist(new Bid(3.0f, item));
-            entityManager.persist(new Bid(5.0f, item));
-            entityManager.persist(new Bid(6.0f, item));
+        });
+
+        // Раздельное управление независимыми
+        // экземплярами сущностей
+        QueryProcessor.process(entityManager -> {
+            final Item itemUpdate = entityManager.createQuery("from Item i where i.id = " + item.id, Item.class)
+                    .getSingleResult();
+            final Bid v1 = new Bid(1.0f, item);
+            itemUpdate.getBids().add(v1);
+            final Bid v2 = new Bid(3.0f, item);
+            itemUpdate.getBids().add(v2);
+            final Bid v3 = new Bid(5.0f, item);
+            itemUpdate.getBids().add(v3);
+            final Bid v4 = new Bid(6.0f, item);
+            itemUpdate.getBids().addAll(Arrays.asList(v1, v2, v3, v4));
+
+            entityManager.persist(v1);
+            entityManager.persist(v2);
+            entityManager.persist(v3);
+            entityManager.persist(v4);
         });
 
         QueryProcessor.process(entityManager -> {
