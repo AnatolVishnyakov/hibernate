f32bd4914b40be06b23fa61d09dffd7c7243eb2c
Anatol V <malfenius@gmail.com>
2020-11-10 23:17:16 +0300

    Улучшаем схему БД. Добавление вспомогательных объектов БД

---
+++ hibernate-ex/src/main/java/com/orm/hibernate/ex/model/complexschemas/CustomSchema.java
@@ -0,0 +1,20 @@
+package com.orm.hibernate.ex.model.complexschemas;
+
+import org.hibernate.boot.model.relational.AbstractAuxiliaryDatabaseObject;
+import org.hibernate.dialect.Dialect;
+
+public class CustomSchema extends AbstractAuxiliaryDatabaseObject {
+    public CustomSchema() {
+        addDialectScope("org.hibernate.dialect.PostgreSQLDialect");
+    }
+
+    @Override
+    public String[] sqlCreateStrings(Dialect dialect) {
+        return new String[]{"[CREATE statement]"};
+    }
+
+    @Override
+    public String[] sqlDropStrings(Dialect dialect) {
+        return new String[]{"[DROP statement]"};
+    }
+}
