2196bdfe8f3721bbe5c522f438e024712966c272
Anatol V <malfenius@gmail.com>
2021-07-11 18:09:42 +0300

    Example PostgreSQL 10

---
+++ example-postgresql/src/main/resources/scheme.sql
@@ -0,0 +1,14 @@
+create database sandbox;
+
+create table toys
+(
+    id    serial  not null,
+    name  character varying(36),
+    usage integer not null default 0,
+    constraint toys_pkey primary key (id)
+);
+
+insert into toys(name)
+values ('car'),
+       ('digger'),
+       ('shovel');
